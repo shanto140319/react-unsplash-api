c91bcafa4258d505c4bbe16860e5cf08bafcd0bc
shanto140319 <shanto140319@gmail.com>
2021-03-18 14:03:13 +0600

    complete

---
+++ src/data.js
@@ -0,0 +1,32 @@
+import React from 'react'
+export const category = [
+  {
+    id: 1,
+    text: 'home',
+  },
+  {
+    id: 2,
+    text: 'cat',
+  },
+  {
+    id: 3,
+    text: 'dog',
+  },
+  {
+    id: 4,
+    text: 'wallpaper',
+  },
+  {
+    id: 5,
+    text: 'nature',
+  },
+    {
+    id: 6,
+    text: 'people',
+    
+  },
+    {
+    id: 7,
+    text: 'fashion',
+  },
+]
